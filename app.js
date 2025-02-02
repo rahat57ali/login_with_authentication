5af9387a6a3d975587f8edf87f81e949d69ecf8e
Rahat <rahatalisoomro10@gmail.com>
2025-02-02 18:46:41 +0500

    Global variables - Custom middleware

--- app.js
+++ app.js
@@ -34,7 +34,12 @@ app.use(session({
 // Connect flash
 app.use(flash());
 
-
+// Global Vars - Custom Middleware
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 
 // Routes
